b2e0d9c9b888df94c7695407808f402e1e96eeec
maimunawahaibi <maimuna.wahaibi@gmail.com>
2021-10-21 17:07:34 +0400

    window center & width

--- backend/segment/helpers.py
+++ backend/segment/helpers.py
@@ -36,3 +36,16 @@ def encode_img_to_string(numpy_img):
     coded_image = base64.b64encode(encoded_img).decode('utf-8')
     return coded_image
 
+def get_min_max_of_window_value(window_center, window_width):
+    min_value= int(window_center) - (int(window_width) // 2)
+    max_value= int(window_center) + (int(window_width) //2 )
+    return min_value,max_value
+
+def window_image(img, window_center, window_width,IMAGE_SIZES):
+    img_min, img_max = get_min_max_of_window_value(window_center, window_width)
+    try:
+        img[img < img_min] = img_min
+        img[img > img_max] = img_max
+    except :
+        img = img_min * np.ones(IMAGE_SIZES)
+    return img
