9a3ce043f5605306a0e3b6d8649bde36e7c84401
NoussaOns <nosaadel99@gmail.com>
2021-11-16 09:45:38 +0400

    useImageUploader Hook Add DragAndDrop Component

--- frontend/src/components/Layout/Sidebar/UploadForm.js
+++ frontend/src/components/Layout/Sidebar/UploadForm.js
@@ -1,47 +1,17 @@
 import React from "react";
-import axios from "axios";
-import {
-  imgActions,
-  segmentedActions,
-  classificationActions,
-} from "../../../store";
-import { useDispatch, useSelector } from "react-redux";
-import { UPLOAD_URL } from "../../../global/endpoints";
+
+import { useSelector } from "react-redux";
+import useImageUploader from "../../../hooks/useImageUploader";
 
 const UploadForm = () => {
-  const dispatch = useDispatch();
+
+  const imgUploader = useImageUploader();
 
   const hiddenFileInput = React.useRef();
 
   const isClassificationLoading = useSelector(
     (state) => state.classification.isLoading
   );
-
-  const imgUploader = (image) => {
-    const uploadData = new FormData();
-    uploadData.append("dcmimg", image);
-    axios
-      .post(UPLOAD_URL, uploadData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      })
-      .then((res) => {
-        // console.log(res.data.image);
-        dispatch(imgActions.setImg(res.data.image));
-        dispatch(imgActions.setIsLoadedImg(true));
-        dispatch(imgActions.setMetadata(JSON.parse(res.data.metadata)));
-
-        dispatch(segmentedActions.setSegmentedImg(res.data.image));
-        dispatch(segmentedActions.setIsSegmented(false));
-        dispatch(segmentedActions.setIsLoading(false));
-        dispatch(segmentedActions.setMarksArray([]));
-        dispatch(segmentedActions.setMarkersActualCoor([]));
-        dispatch(segmentedActions.setHistogram([]));
-        dispatch(segmentedActions.setStatistics([]));
-
-        dispatch(classificationActions.setIsClassified(false));
-      })
-      .catch((err) => console.error(err));
-  };
 
   return (
     <>
